c2d1e5d265cec4fe59e4f0a715d2349907d1d88a
John Daratzikis <johndaratzikis@johns-macbook-pro.local>
2017-03-01 12:26:20 +0200

    version 1.0

--- DigitalClock/ViewController.h
+++ DigitalClock/ViewController.h
@@ -8,8 +8,19 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController {
 
+    NSTimer *timer;
+
+}
+
+@property (weak, nonatomic) IBOutlet UILabel *label;
+@property (weak, nonatomic) IBOutlet UIView *settingView;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *clockColorSegment;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *backgroundColorSegment;
+- (IBAction)settingsButton:(id)sender;
+- (IBAction)clockChange:(id)sender;
+- (IBAction)backgroundChange:(id)sender;
 
 @end
 
--- DigitalClock/ViewController.m
+++ DigitalClock/ViewController.m
@@ -17,6 +17,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.label.text = @"";
+    self.settingView.hidden = YES;
+    
+    timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(updateClock) userInfo:nil repeats:YES];
 }
 
 
@@ -25,5 +30,70 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)updateClock {
 
+    NSDateFormatter *formatter = [[NSDateFormatter alloc]init];
+    [formatter setDateFormat:@"hh:mm:ss"];
+    self.label.text = [formatter stringFromDate:[NSDate date]];
+
+}
+
+
+- (IBAction)settingsButton:(id)sender {
+
+    if (self.settingView.hidden == NO) {
+    
+        self.settingView.hidden = YES;
+        
+    }else {
+    
+        self.settingView.hidden = NO;
+    
+    }
+    
+}
+
+- (IBAction)clockChange:(id)sender {
+    
+    if (self.clockColorSegment.selectedSegmentIndex == 0){
+    
+        self.label.textColor = [UIColor whiteColor];
+        
+    } else if (self.clockColorSegment.selectedSegmentIndex == 1){
+        
+        self.label.textColor = [UIColor blackColor];
+        
+    }else if (self.clockColorSegment.selectedSegmentIndex == 2){
+        
+        self.label.textColor = [UIColor greenColor];
+        
+    } else if (self.clockColorSegment.selectedSegmentIndex == 3){
+        
+        self.label.textColor = [UIColor redColor];
+        
+    }
+    
+}
+
+- (IBAction)backgroundChange:(id)sender {
+    
+    if (self.backgroundColorSegment.selectedSegmentIndex == 0){
+        
+        self.view.backgroundColor = [UIColor blackColor];
+        
+    } else if (self.backgroundColorSegment.selectedSegmentIndex == 1){
+        
+        self.view.backgroundColor = [UIColor whiteColor];
+        
+    }else if (self.backgroundColorSegment.selectedSegmentIndex == 2){
+        
+        self.view.backgroundColor = [UIColor blueColor];
+        
+    } else if (self.backgroundColorSegment.selectedSegmentIndex == 3){
+        
+        self.view.backgroundColor = [UIColor yellowColor];
+        
+    }
+    
+}
 @end
